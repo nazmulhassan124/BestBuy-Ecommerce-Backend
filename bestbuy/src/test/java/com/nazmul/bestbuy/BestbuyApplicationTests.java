fa68d7adabad713c5e57a641253e5f166dca25e2
Nazmul Hassan <106418134+nazmulhassan124@users.noreply.github.com>
2023-06-23 12:07:57 +0600

    first commit

---
+++ bestbuy/src/test/java/com/nazmul/bestbuy/BestbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazmul.bestbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
