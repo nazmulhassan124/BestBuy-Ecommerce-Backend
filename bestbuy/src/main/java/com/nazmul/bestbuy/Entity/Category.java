27dc4ead1bb564b8d07d7a8d5111d4462c08d764
Nazmul Hassan <106418134+nazmulhassan124@users.noreply.github.com>
2023-07-02 00:30:04 +0600

    vendor model

--- bestbuy/src/main/java/com/nazmul/bestbuy/Entity/Category.java
+++ bestbuy/src/main/java/com/nazmul/bestbuy/Entity/Category.java
@@ -16,30 +16,7 @@ public class Category {
     private String catname;
     private String catdesc;
     private String catimage;
-    public long getCatid() {
-        return catid;
-    }
-    public void setCatid(long catid) {
-        this.catid = catid;
-    }
-    public String getCatname() {
-        return catname;
-    }
-    public void setCatname(String catname) {
-        this.catname = catname;
-    }
-    public String getCatdesc() {
-        return catdesc;
-    }
-    public void setCatdesc(String catdesc) {
-        this.catdesc = catdesc;
-    }
-    public String getCatimage() {
-        return catimage;
-    }
-    public void setCatimage(String catimage) {
-        this.catimage = catimage;
-    }
+
 
 
 
--- bestbuy/src/main/java/com/nazmul/bestbuy/Entity/Vendor.java
+++ bestbuy/src/main/java/com/nazmul/bestbuy/Entity/Vendor.java
@@ -16,26 +16,39 @@ public class Vendor {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long vendorId;
     private String vendorName;
+
+    private String email;
     private String vendordesc;
 
     public long getVendorId() {
         return vendorId;
     }
+
     public void setVendorId(long vendorId) {
         this.vendorId = vendorId;
     }
+
     public String getVendorName() {
         return vendorName;
     }
+
     public void setVendorName(String vendorName) {
         this.vendorName = vendorName;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getVendordesc() {
         return vendordesc;
     }
+
     public void setVendordesc(String vendordesc) {
         this.vendordesc = vendordesc;
     }
-
-
 }
